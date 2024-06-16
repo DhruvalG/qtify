e9be8853dab91601b651e54e0e6cbef5210c1417
DhruvalG <dhruval.gv@gmail.com>
2024-06-17 01:17:41 +0530

    Added a NavBar

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CompanyLogo from "../../assets/companyLogo.png"
+const Logo = () => {
+    return (
+        <div>
+            <img src={CompanyLogo} alt="Logo" width={67}/>
+        </div>
+    )
+}
+export default Logo;
