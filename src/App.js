6c26b80ba11e23ddf4cc48fe81c96f8a15dc26ca
DhruvalG <dhruval.gv@gmail.com>
2024-06-18 00:04:28 +0530

    Added Songs, Albums and Playlist Grids, Added Carousel and Added Filters from Figma

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState} from 'react'
-import { HeroSection, NavBar } from './components';
-import { fetchTopAlbums } from './api/api';
+import { HeroSection, NavBar, AlbumPlaylistGrids, SongsSectionDivider } from './components';
+import { fetchNewAlbums, fetchTopAlbums, fetchSongs } from './api/api';
 const App = () => {
   const [dataTopAlbums, setDataTopAlbums] = useState([]);
-
+  const [dataNewAlbum, setDataNewAlbum] = useState([]);
+  const [dataSong, setDataSong] = useState([]);
   const getDataTopAlbums = async () => {
     try {
       const res = await fetchTopAlbums();
@@ -12,15 +13,37 @@ const App = () => {
       console.error(error)
     }
   }
-
+  const getDataNewAlbums = async () => {
+    try {
+      const res = await fetchNewAlbums();
+      setDataNewAlbum(res);
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  const getDataSongs = async () => {
+    try {
+      const res = await fetchSongs();
+      setDataSong(res);
+    } catch (error) {
+      console.error(error)
+    }
+  }
   useEffect(() => {
     getDataTopAlbums();
+    getDataNewAlbums();
+    getDataSongs()
   }, [])
 
   return (
     <div>
       <NavBar data={dataTopAlbums} />
       <HeroSection />
+      <AlbumPlaylistGrids title={"Top Albums"} data={dataTopAlbums} type={"album"} />
+      <AlbumPlaylistGrids title={"New Albums "} data={dataNewAlbum} type={"album"} />
+      <SongsSectionDivider />
+      <AlbumPlaylistGrids title={"Songs "} data={dataSong} type={"song"} setDataSong={setDataSong} />
+      <SongsSectionDivider />
     </div>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,12 @@ export { default as NavBar } from './NavBar/NavBar.jsx';
 export { default as HeroSection } from './HeroSection/HeroSection.jsx';
 export { default as SearchBar } from './SearchBar/SearchBar.jsx';
 export { default as SearchFunct } from './SearchBar/SearchFunct.jsx';
+export { default as SongsSectionDivider } from './SongsSectionDivider/SongsSectionDivider.jsx';
 
 export { default as Logo } from './Logo/Logo.jsx';
 export { default as Buttons } from './Button/Button.jsx';
 export { default as FeedbackButton } from './FeedbackButton/FeedbackButton.jsx';
+export { default as AlbumPlaylistCardChildrens } from './AlbumPlaylistCards/AlbumPlaylistCardChildrens/AlbumPlaylistCardChildrens.jsx';
+export { default as MainAlbumPlaylistCards } from './AlbumPlaylistCards/MainAlbumPlaylistCards/MainAlbumPlaylistCards.jsx';
+export { default as AlbumPlaylistGrids } from './AlbumPlaylistGrids/AlbumPlaylistGrids.jsx';
+export { default as AlbumPlaylistCardsCarousel } from './AlbumPlaylistCards/AlbumPlaylistCardCarousels/AlbumPlaylistCardsCarousel.jsx';
